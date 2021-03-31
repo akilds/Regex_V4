2e9b8efe5a3f11380de0d94d37858235bf67c059
akilds <akilrainadesigan@gmail.com>
2021-03-31 11:14:48 +0530

    ADDED ; Firstname Check

--- regex.java
+++ regex.java
@@ -1,7 +1,31 @@
+import java.util.regex.Pattern;
+import java.util.*;
+
 public class regex
 {
+ // USE CASE 1 - Validity Check of  Firstname
+ static String getFirstName()
+ {
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Enter Firstname : ");
+  String fname = sc.nextLine();
+  return fname;
+ }
+ static void firstNameCheck()
+ {
+  String fname = getFirstName();
+  boolean nameCheck = Pattern.matches("^[A-Z][a-z]{2,}",fname);
+  if(nameCheck)
+  {
+   System.out.println("Correct");
+  }
+  else
+  {
+   System.out.println("Invalid Firstname - Enter a name starting with Cap and has minimum 3 characters");
+  }
+ }
  public static void main(String[] args)
  {
-  System.out.println("User Registration Problem");
+  firstNameCheck();
  }
 }
