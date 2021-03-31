eeee051a191f658831508c2ccd64662023157a3b
akilds <akilrainadesigan@gmail.com>
2021-03-31 20:11:20 +0530

    ADDED : Password Validity

--- regex.java
+++ regex.java
@@ -65,6 +65,8 @@ public class regex
   }
  }
 
+ //USE CASE 4 - Validity Check for Mobile Number
+ //Moblie Number User Input
  static String getMobileNo()
  {
   Scanner sc = new Scanner(System.in);
@@ -73,6 +75,7 @@ public class regex
   return mobileNo;
  }
 
+ //Check for Mobile Number
  static void mobileNoCheck()
  {
   String mobileNo = getMobileNo();
@@ -87,10 +90,36 @@ public class regex
   }
  }
 
+ //USE CASE 5 - Validity Check for Password
+ //PAssword User Input
+ static String getPassword()
+ {
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Enter Password : ");
+  String password = sc.nextLine();
+  return password;
+ }
+
+ //Check for Password
+ static void passwordCheck()
+ {
+  String password = getPassword();
+  boolean passCheck = Pattern.matches("^[a-z]{8,}",password);
+  if(passCheck)
+  {
+   System.out.println("Valid Password");
+  }
+  else
+  {
+   System.out.println("Invalid Password");
+  }
+ }
+
  public static void main(String[] args)
  {
-  nameCheck();
-  emailCheck();
-  mobileNoCheck();
+  //nameCheck();
+  //emailCheck();
+  //mobileNoCheck();
+  passwordCheck();
  }
 }
